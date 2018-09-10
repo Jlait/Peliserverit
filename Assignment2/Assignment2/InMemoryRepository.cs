a428d829dad38088eb01424aaaf3d8ddce4ea4c7
J.laitinen <31722113+Jlait@users.noreply.github.com>
2018-09-10 19:21:38 +0300

    joo
    yeaboi

---
+++ Assignment2/Assignment2/InMemoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class InMemoryRepository : IRepository
+    {
+        List<Player> playerList = new List<Player>();
+
+        public InMemoryRepository() { }
+
+        public Task<Player> Create(Player player)
+        {
+            playerList.Add(player);
+            return Task.FromResult(player);
+        }
+
+        public Task<Player> Delete(Guid id)
+        {
+            foreach (Player p in playerList)
+            {
+                if (p.Id == id)
+                {
+                    playerList.Remove(p);
+                    return Task.FromResult(p);
+                }
+            }
+            return null; 
+        }
+
+        public Task<Player> Get(Guid id)
+        {
+            foreach (Player p in playerList)
+            {
+                if (p.Id == id)
+                {
+                    return Task.FromResult(p);
+                }
+            }
+            return null;
+        }
+
+        public Task<Player[]> GetAll()
+        {
+            return Task.FromResult(playerList.ToArray());
+        }
+
+        public Task<Player> Modify(Guid id, ModifiedPlayer player)
+        {
+            foreach (Player p in playerList)
+            {
+                if (p.Id == id)
+                {
+                    p.Score = player.Score;
+                    return Task.FromResult(p);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Assignment2/Assignment2/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Player
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public bool IsBanned { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
